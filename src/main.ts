e2b9c1784cf52bb06b2b93ef5a30bceb6b3659ce
Zegue Kurt <zegueleonel@gmail.com>
2022-04-02 10:00:11 +0100

    chore: add log level

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger:
+      process.env.LOG_LEVEL === 'debug'
+        ? ['debug', 'error', 'warn']
+        : ['error', 'warn'],
+  });
   await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
